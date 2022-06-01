3d9f8fcbd8e797b2c5dff714577af3a01c165ead
Michał Robakowski <48829502+michaus736@users.noreply.github.com>
2022-06-01 23:54:55 +0200

    modify

--- Multithread JPEG Codec/Program.cs
+++ Multithread JPEG Codec/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Multithread_JPEG_Codec;
+
+
+
+
+//testing
+
+var test = new TestingClass(TestingClass.builderAcceleratorType.Cpu);
