6ed6db552785122885fc406cb516d62b5ffbb123
Michał Robakowski <48829502+michaus736@users.noreply.github.com>
2022-06-04 16:21:06 +0200

    changing logging and add publish properties

--- Multithread JPEG Codec/Program.cs
+++ Multithread JPEG Codec/Program.cs
@@ -4,5 +4,22 @@
 
 
 //testing
+string filePath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "devices_list.txt";
+//Stream consoleStream = Console.OpenStandardOutput();
+FileStream fileStream = File.Open(filePath, FileMode.Append, FileAccess.Write);
 
-var test = new TestingClass(TestingClass.builderAcceleratorType.Cpu);
+Stream stream = fileStream;
+StreamWriter sw = new StreamWriter(stream);
+
+TestingClass.printDevices(sw, TestingClass.builderAcceleratorType.Cpu);
+TestingClass.printDevices(sw, TestingClass.builderAcceleratorType.OpenCL);
+
+if(stream is FileStream)
+{
+    Console.WriteLine($"appeding file in: {filePath}");
+}
+
+
+sw.Dispose();
+Console.WriteLine("press key to exit");
+Console.ReadKey();
