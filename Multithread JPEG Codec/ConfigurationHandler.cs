87ccb9bbce68c64cd37ee70240f57674b635c1ab
Michał Robakowski <48829502+michaus736@users.noreply.github.com>
2023-01-11 01:14:46 +0100

    changing program.cs, adding implementation with custom region number, modifying args, add publish profiles

---
+++ Multithread JPEG Codec/ConfigurationHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithread_JPEG_Codec;
+
+internal static class ConfigurationHandler
+{
+    public static Dictionary<string, string> ConfigureArgs(this Dictionary<string, string> keyValuePairs, string[] args)
+    {
+        if(args.Length % 2 != 0) throw new ArgumentException("argument aren't in pairs"); 
+        Dictionary<string, string> result = new Dictionary<string, string>();
+        
+        
+        for(int i = 0; i < args.Length; i += 2)
+        {
+            string key = args[i];
+            string value = args[i + 1];
+            if (!key.StartsWith("--")) throw new ArgumentException("keys of values always starts with '--' prefix");
+            key = key.TrimStart('-');
+            result.Add(key, value);
+        }
+        return result;
+    }
+}
